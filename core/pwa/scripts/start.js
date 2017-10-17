2840dfd248815e935886b5f70b800d471f5e542d
Luis Herranz <luisherranz@gmail.com>
2017-10-17 13:14:50 +0200

    Use the same cors headers in both dev and prod

--- core/pwa/scripts/start.js
+++ core/pwa/scripts/start.js
@@ -33,8 +33,7 @@ const startDev = async () => {
     stats: { colors: true },
     headers: {
       'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
     },
   };
 
